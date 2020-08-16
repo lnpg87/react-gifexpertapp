b10e64a031423b2f5308788c7a8f354a8228f077
Luis <lnpg87@gmail.com>
2020-08-16 13:32:49 -0400

    Docs agregado

---
+++ docs/precache-manifest.e4400119b17f17e962738a32b09d0d67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b778be249cc2a0df1e4af31154fb19e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ad549a4d10e2c2a35cc",
+    "url": "/static/css/main.29f3dc91.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ad549a4d10e2c2a35cc",
+    "url": "/static/js/main.cf90f5a0.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
